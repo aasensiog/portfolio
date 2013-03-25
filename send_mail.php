1042aa4f1725adb7b9fcab408383ad87512299fb
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-03-25 16:08:29 +0100

    form

---
+++ send_mail.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST["email"]<>'') {
+    $ToEmail = 'me@albertasensio.es';
+    $EmailSubject = 'Site contact form';
+    $mailheader = "From: ".$_POST["email"]."\r\n";
+    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
+    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
+    $MESSAGE_BODY = "Name: ".$_POST["name"]."";
+    $MESSAGE_BODY .= "Email: ".$_POST["email"]."";
+    $MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"])."";
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
+?>
+<?php
