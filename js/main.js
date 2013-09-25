c30a68e3cba48a47d8a47347eaf344e1b4a2c214
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-09-25 17:30:45 +0200

    starting bootstrap integration

--- js/main.js
+++ js/main.js
@@ -2,39 +2,18 @@
 $(document).ready(function() {
   $('.error').hide();
   $('#send_mail').click(function() {
-    // validate and process form here
-
-    $('.error').hide();
       var name = $('input#name').val();
-      if (name == '') {
-        $('label#name_error').show();
-        $('input#name').focus();
-        return false;
-      }
       var email = $('input#email').val();
-      if (email == '') {
-        $('label#email_error').show();
-        $('input#email').focus();
-        return false;
-      }
       var comment = $('textarea#comment').val();
-      if (comment == '') {
-        $('label#comment_error').show();
-        $('input#comment').focus();
-        return false;
-      }
 
       var dataString = 'name='+ name + '&email=' + email + '&comment=' + comment;
-      //alert (dataString);return false;
+      
       $.ajax({
         type: 'POST',
         url: 'bin/send_mail.php',
         data: dataString,
         success: function() {
-          $('#contact_form').html("'<div id='message'></div>");
-          $('#message').html('<h3>Correo enviado correctamente</h3>')
-          .append('<p>Me pondré en contacto en breve.</p>')
-          .hide()
+          $('#contact_form').html("'<div id='message'><h3>Correo enviado correctamente</h3><p>Me pondré en contacto en breve.</p></div>");
           .fadeIn(1500, function() {
             $('#message').append("<img id='checkmark' src='img/check.png' />");
           });
